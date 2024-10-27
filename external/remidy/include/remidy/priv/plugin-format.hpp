8a2d7175fb1b53a8f6102953b548a76fa61b1b16
Atsushi Eno <atsushieno@gmail.com>
2024-10-28 00:06:27 +0900

    more comments on AudioPluginFormat class.

--- external/remidy/include/remidy/priv/plugin-format.hpp
+++ external/remidy/include/remidy/priv/plugin-format.hpp
@@ -17,16 +17,29 @@ namespace remidy {
 
         virtual AudioPluginExtensibility<AudioPluginFormat>* getExtensibility() { return nullptr; }
 
+        // Indicates whether you should invoke plugin methods on the UI/main thread.
+        //
+        // VST3 and CLAP requires almost all plugin operations performed on the main thread, while
+        // AudioUnit and LV2 don't. This impacts the overall app performance (take it like you have
+        // a GIL in Python, or forced single-threaded COM model). Also, in those free-threaded model
+        // you have to pay attention to accesses to the plugin resources (you should).
+        //
+        // This method is marked as virtual so that you can actually override this and opt in to
+        // process certain VST/CLAP plugins work in better "multi thread ready" way, or on the other
+        // hand treat some AU/LV2 plugins work only on the main thread when any of plugins in those
+        // formats behave unstable.
         virtual AudioPluginUIThreadRequirement requiresUIThreadOn(PluginCatalogEntry* entry) = 0;
 
         bool hasPluginListCache();
         bool supportsGlobalIdentifier();
+
         // Indicates whether the plugin API requires sample rate at *instantiating*.
         // Only LV2 requires this, among VST3, AUv2/v3, LV2, and CLAP.
         // This impacts on whether we will have to discard and instantiate a plugin
         // when our use app changes the sample rate.
         bool instantiateRequiresSampleRate();
 
+        // Indicates that scanning of the plugins in this format is based on file paths (VST3, LV2, CLAP vs. AU).
         virtual bool usePluginSearchPaths() = 0;
         virtual std::vector<std::filesystem::path>& getDefaultSearchPaths() = 0;
         enum class ScanningStrategyValue {
@@ -34,8 +47,14 @@ namespace remidy {
             MAYBE,
             ALWAYS
         };
+        // Indicates whether scanning of the plugin requires loading of the library.
+        // `ALWAYS` for CLAP, `MAYBE` for VST3 (when `moduleinfo.json` does not exist), `NONE` for AU and LV2.
         virtual ScanningStrategyValue scanRequiresLoadLibrary() = 0;
+        // Indicates whether scanning of the plugin requires instancing of the plugin IF it needs to load the library.
+        // NEVER for AU, LV2, and CLAP. ALWAYS for VST3.
         virtual ScanningStrategyValue scanRequiresInstantiation() = 0;
+        // Implements plugin scanning. You might want to use this functionality in a separate process as
+        // some bad behaving plugins may crash the process.
         virtual std::vector<std::unique_ptr<PluginCatalogEntry>> scanAllAvailablePlugins() = 0;
 
         struct InvokeResult {
