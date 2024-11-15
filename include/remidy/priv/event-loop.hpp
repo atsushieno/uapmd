dc37fcb7d8f683d5a1251b5f7ca62ff2943b41b4
Atsushi Eno <atsushieno@gmail.com>
2024-11-15 23:20:19 +0900

    Make EventLoop pluggable i.e. implementatble on top of any UI framework.

--- include/remidy/priv/event-loop.hpp
+++ include/remidy/priv/event-loop.hpp
@@ -3,17 +3,38 @@
 #include <functional>
 
 namespace remidy {
-
+    // Provides the way for plugin UI support implementation to get access to UI event loop
+    // so that it can appropriately dispatch some function invocation on the UI thread.
+    //
+    // Call `initializeOnUIThread()` once, and `start()` to run the main UI message loop.
+    // It will then go into infinite message loop until `stop()` is invoked by some event
+    // (or externally invoked on another thread).
+    //
+    // It is designed to be "pluggable" so that the actual hosting application can work with
+    // any UI framework that offers way to initialize event loop and dispatch UI events.
+    // Implement (derive from) this class and call `instance(EventLoop)` to use it.
+    // Make sure to do this before invoking `initializeOnUIThread()` (or any other functions).
+    //
+    // The default implementation (exists) is based on choc (`choc::gui::MessageLoop`).
     class EventLoop {
+    protected:
+        virtual void initializeOnUIThreadImpl() = 0;
+        virtual bool runningOnMainThreadImpl() = 0;
+        virtual void runTaskOnMainThreadImpl(std::function<void()>&& func) = 0;
+        virtual void enqueueTaskOnMainThreadImpl(std::function<void()>&& func) = 0;
+        virtual void startImpl() = 0;
+        virtual void stopImpl() = 0;
     public:
-        static void initializeOnUIThread();
-        static bool runningOnMainThread();
+        static EventLoop* instance();
+        static void instance(EventLoop& newImpl);
+        static void initializeOnUIThread() { instance()->initializeOnUIThreadImpl(); }
+        static bool runningOnMainThread() { return instance()->runningOnMainThreadImpl(); }
         // Run task either immediately (if current thread is the main thread) or asynchronously (otherwise).
-        static void runTaskOnMainThread(std::function<void()>&& func);
+        static void runTaskOnMainThread(std::function<void()>&& func) { instance()->runTaskOnMainThreadImpl(std::move(func)); }
         // Enqueue task on the main thread to run asynchronously.
-        static void enqueueTaskOnMainThread(std::function<void()>&& func);
-        static void start();
-        static void stop();
+        static void enqueueTaskOnMainThread(std::function<void()>&& func) { instance()->enqueueTaskOnMainThreadImpl(std::move(func)); }
+        static void start() { instance()->startImpl(); }
+        static void stop() { instance()->stopImpl(); }
     };
 
 }
--- src/remidy/EventLoop.cpp
+++ src/remidy/EventLoop.cpp
@@ -3,27 +3,39 @@
 
 // FIXME: maybe we will not use choc on Linux as it depends on Gtk which is in principle unacceptable.
 namespace remidy {
-    void EventLoop::initializeOnUIThread() {
-        choc::messageloop::initialise();
-    }
-    bool EventLoop::runningOnMainThread() {
-        return choc::messageloop::callerIsOnMessageThread();
-    }
+    class EventLoopChoc : public EventLoop {
+    protected:
+        void initializeOnUIThreadImpl() override {
+            choc::messageloop::initialise();
+        }
+        bool runningOnMainThreadImpl() override {
+            return choc::messageloop::callerIsOnMessageThread();
+        }
 
-    void EventLoop::runTaskOnMainThread(std::function<void()>&& func) {
-        if (choc::messageloop::callerIsOnMessageThread())
-            func();
-        else
+        void runTaskOnMainThreadImpl(std::function<void()>&& func) override {
+            if (choc::messageloop::callerIsOnMessageThread())
+                func();
+            else
+                choc::messageloop::postMessage(std::move(func));
+        }
+        void enqueueTaskOnMainThreadImpl(std::function<void()>&& func) override {
             choc::messageloop::postMessage(std::move(func));
-    }
-    void EventLoop::enqueueTaskOnMainThread(std::function<void()>&& func) {
-        choc::messageloop::postMessage(std::move(func));
-    }
-    void EventLoop::start() {
-        choc::messageloop::run();
-    }
+        }
+        void startImpl() override {
+            choc::messageloop::run();
+        }
 
-    void EventLoop::stop() {
-        choc::messageloop::stop();
+        void stopImpl() override {
+            choc::messageloop::stop();
+        }
+    };
+
+    EventLoopChoc choc{};
+    EventLoop* impl{&choc};
+    EventLoop* EventLoop::instance() {
+        return impl;
+    }
+    void EventLoop::instance(EventLoop& newImpl) {
+        impl = &newImpl;
     }
 }
