e3d8f999495a627b35f39b9259585a4512475485
Atsushi Eno <atsushieno@gmail.com>
2024-12-09 14:12:54 +0900

    fix project build.

--- tools/uapmd-service/VirtualMidiDevices/UapmdMidiDevice.cpp
+++ tools/uapmd-service/VirtualMidiDevices/UapmdMidiDevice.cpp
@@ -5,7 +5,8 @@ namespace uapmd {
 
     UapmdMidiDevice::UapmdMidiDevice(std::string& deviceName, std::string& manufacturer, std::string& version) :
         platformDevice(new PlatformVirtualMidiDevice(deviceName, manufacturer, version)),
-        audioPluginHost(new AudioPluginSequencer(AudioPluginHostPAL::instance())) {
+        // FIXME: do we need valid sampleRate here?
+        audioPluginHost(new AudioPluginSequencer(44100, AudioPluginHostPAL::instance())) {
     }
 
 }
