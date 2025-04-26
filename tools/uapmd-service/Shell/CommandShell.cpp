d41f9bf5505f97cdcb86610c5a654ce3df2c159d
Atsushi Eno <atsushieno@gmail.com>
2025-04-27 00:28:52 +0900

    uapmd-service: do not start audio processing on the main thread.

--- tools/uapmd-service/Shell/CommandShell.cpp
+++ tools/uapmd-service/Shell/CommandShell.cpp
@@ -18,17 +18,30 @@ namespace uapmd {
     }
 
     int CommandShell::run() {
-        controller = std::make_unique<VirtualMidiDeviceController>();
+        remidy::EventLoop::initializeOnUIThread();
+
         const auto& deviceName = plugin_name.size() ? plugin_name : "UAPMD (Any Plugin)";
         Logger::global()->logInfo("Starting %s", deviceName.c_str());
-        device = controller->createDevice(deviceName, "UAPMD Project", "0.1");
-        device->addPluginTrack(plugin_name, format_name);
 
-        device->start();
+        // FIXME: run in realtime audio (priority) thread
+        std::thread t([&] {
+            controller = std::make_unique<VirtualMidiDeviceController>();
+            device = controller->createDevice(deviceName, "UAPMD Project", "0.1");
+            device->addPluginTrack(plugin_name, format_name);
 
-        std::cerr << "Type [CR] to quit." << std::endl;
-        std::cin.get();
+            device->start();
+        });
 
+        std::thread t2([&] {
+            std::cerr << "Type [CR] to quit." << std::endl;
+            std::cin.get();
+            remidy::EventLoop::stop();
+        });
+
+        remidy::EventLoop::start();
+
+        t2.join();
+        t.join();
         device->stop();
 
         return 0;
