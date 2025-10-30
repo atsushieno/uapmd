4df8e2ddbc69c73156f9d3438e495fd5cde79eeb
Atsushi Eno <atsushieno@gmail.com>
2025-10-30 14:23:50 +0900

    implement preset support in AU (Codex, like 25% as it was too buggy)

--- src/remidy/au/PluginInstanceAU.Presets.mm
+++ src/remidy/au/PluginInstanceAU.Presets.mm
@@ -1,29 +1,59 @@
 #if __APPLE__
 
 #include "PluginFormatAU.hpp"
-
-#include "PluginFormatAU.hpp"
+#include <stdexcept>
+#include <choc/platform/choc_ObjectiveCHelpers.h>
 
 using namespace remidy;
 
 PluginInstanceAU::PresetsSupport::PresetsSupport(PluginInstanceAU* owner) : owner(owner) {
+    CFArrayRef presets = nullptr;
+    UInt32 size = sizeof(CFArrayRef);
+    auto err = AudioUnitGetProperty(owner->instance, kAudioUnitProperty_FactoryPresets, kAudioUnitScope_Global, 0, &presets, &size);
+    if (err != noErr)
+        return;
+
+    auto presetCount = CFArrayGetCount(presets);
+    for (CFIndex i = 0; i < presetCount; i++) {
+        auto preset = (const AUPreset *) CFArrayGetValueAtIndex(presets, i);
+        auto nsPresetName = (__bridge NSString *) preset->presetName;
+        auto presetName = choc::objc::getString(nsPresetName);
+
+        std::string id = std::to_string(preset->presetNumber);
+        items.emplace_back(id, presetName, 0, preset->presetNumber);
+    }
 }
 
 int32_t PluginInstanceAU::PresetsSupport::getPresetIndexForId(std::string &id) {
-    throw std::runtime_error("FIXME: not implemented");
+    for (int32_t i = 0, n = (int32_t) items.size(); i < n; i++) {
+        if (items[i].id() == id)
+            return i;
+    }
+    return -1;
 }
 
 int32_t PluginInstanceAU::PresetsSupport::getPresetCount() {
-    std::cerr << "FIXME: PresetsSupport::getPresetCount not implemented" << std::endl;
-    return 0;
+    return (int32_t) items.size();
 }
 
 PresetInfo PluginInstanceAU::PresetsSupport::getPresetInfo(int32_t index) {
-    throw std::runtime_error("FIXME: not implemented");
+    return items[(size_t) index];
 }
 
 void PluginInstanceAU::PresetsSupport::loadPreset(int32_t index) {
-    throw std::runtime_error("FIXME: not implemented");
+    auto& preset = items[(size_t) index];
+    AUPreset auPreset{};
+    auPreset.presetNumber = preset.index();
+    auPreset.presetName = CFStringCreateWithCString(nullptr, preset.name().c_str(), kCFStringEncodingUTF8);
+
+    auto status = AudioUnitSetProperty(owner->instance, kAudioUnitProperty_PresentPreset,
+                                       kAudioUnitScope_Global, 0, &auPreset, sizeof(auPreset));
+    if (status != noErr)
+        owner->logger()->logWarning("%s: failed to load AU preset %s (%d). Status: %d",
+                                    owner->name.c_str(), preset.name().c_str(), preset.index(), status);
+
+    if (auPreset.presetName)
+        CFRelease(auPreset.presetName);
 }
 
 #endif
