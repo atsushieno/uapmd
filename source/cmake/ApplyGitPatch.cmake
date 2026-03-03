978fefeff6ce33d26d64f6371832c19154c4d7aa
Atsushi Eno <atsushieno@gmail.com>
2026-03-03 18:18:38 +0900

    Codex failed hard to create an appropriate patch for ResEmbed.

--- source/cmake/ApplyGitPatch.cmake
+++ source/cmake/ApplyGitPatch.cmake
@@ -3,14 +3,18 @@ if(NOT DEFINED PATCH_FILE)
 endif()
 
 get_filename_component(_patch_file "${PATCH_FILE}" REALPATH)
-set(_patch_work_dir "${CMAKE_BINARY_DIR}")
+if(DEFINED PATCH_WORK_DIR)
+    get_filename_component(_patch_work_dir "${PATCH_WORK_DIR}" REALPATH)
+else()
+    set(_patch_work_dir "${CMAKE_BINARY_DIR}")
+endif()
 
 if(NOT EXISTS "${_patch_file}")
     message(FATAL_ERROR "Patch file not found: ${_patch_file}")
 endif()
 
 execute_process(
-    COMMAND git apply --reverse --check "${_patch_file}"
+    COMMAND git apply --reverse --check --ignore-whitespace "${_patch_file}"
     WORKING_DIRECTORY "${_patch_work_dir}"
     RESULT_VARIABLE _reverse_result
     OUTPUT_QUIET
@@ -22,7 +26,7 @@ if(_reverse_result EQUAL 0)
 endif()
 
 execute_process(
-    COMMAND git apply --check "${_patch_file}"
+    COMMAND git apply --check --ignore-whitespace "${_patch_file}"
     WORKING_DIRECTORY "${_patch_work_dir}"
     RESULT_VARIABLE _check_result)
 
@@ -31,7 +35,7 @@ if(NOT _check_result EQUAL 0)
 endif()
 
 execute_process(
-    COMMAND git apply "${_patch_file}"
+    COMMAND git apply --ignore-whitespace "${_patch_file}"
     WORKING_DIRECTORY "${_patch_work_dir}"
     RESULT_VARIABLE _apply_result)
 
