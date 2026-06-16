58fea7fcef5985e221bde9cf8dcc8d00843d4af5
Atsushi Eno <atsushieno@gmail.com>
2026-06-16 23:45:48 +0900

    update AAP API to the latest aap-core version.

--- source/remidy/src/aap/PluginFormatAAP.cpp
+++ source/remidy/src/aap/PluginFormatAAP.cpp
@@ -57,12 +57,12 @@ void PluginFormatAAPImpl::createInstance(PluginCatalogEntry *info,
         auto identifier = pluginInfo->getPluginID();
         auto service = plugin_client_connections->getServiceHandleForConnectedPlugin(pluginInfo->getPluginPackageName(), pluginInfo->getPluginLocalName());
         if (service != nullptr) {
-            auto result = android_host->createInstance(identifier, sampleRate, true);
+            auto result = android_host->createInstance(identifier, true);
             aapCallback(result.value, result.error);
         } else {
-            std::function<void(std::string&)> cb = [identifier,sampleRate,aapCallback,this](std::string& error) {
+            std::function<void(std::string&)> cb = [identifier,aapCallback,this](std::string& error) {
                 if (error.empty()) {
-                    auto result = android_host->createInstance(identifier, sampleRate, true);
+                    auto result = android_host->createInstance(identifier, true);
                     aapCallback(result.value, result.error);
                 }
                 else
